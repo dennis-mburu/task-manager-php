23a93ed190154646d009b5094bb8c3b73616a15a
Dennis Mburu <dennismburu2063@gmail.com>
2025-07-12 10:24:51 +0300

    update SQL dump: change deadline from just DATE to DATETIME

--- sql/task_manager.sql
+++ sql/task_manager.sql
@@ -28,13 +28,13 @@ CREATE TABLE `tasks` (
   `title` varchar(255) NOT NULL,
   `description` text DEFAULT NULL,
   `assigned_to` int(11) DEFAULT NULL,
-  `deadline` date DEFAULT NULL,
+  `deadline` datetime DEFAULT NULL,
   `status` enum('Pending','In Progress','Completed') DEFAULT 'Pending',
   `created_at` timestamp NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`),
   KEY `assigned_to` (`assigned_to`),
   CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`assigned_to`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -44,6 +44,12 @@ CREATE TABLE `tasks` (
 LOCK TABLES `tasks` WRITE;
 /*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
 set autocommit=0;
+INSERT INTO `tasks` VALUES
+(1,'Complete Task Manager Project','Have a MVP of a task manager app',2,'2025-07-15 00:00:00','Pending','2025-07-12 06:37:03'),
+(2,'Add email notification','Notify users of account creation',1,'2025-07-15 00:00:00','Pending','2025-07-12 06:37:57'),
+(3,'Polish UI','Style all pages',3,'2025-07-14 00:00:00','Pending','2025-07-12 06:38:28'),
+(4,'Update datetime','Update datetime everywhere',1,'2025-07-12 00:00:00','Pending','2025-07-12 07:04:45'),
+(5,'Update task list ui','add countdown',1,'2025-07-12 00:13:00','Pending','2025-07-12 07:11:10');
 /*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
 UNLOCK TABLES;
 commit;
@@ -63,7 +69,7 @@ CREATE TABLE `users` (
   `role` enum('admin','user') DEFAULT 'user',
   `created_at` timestamp NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -73,6 +79,10 @@ CREATE TABLE `users` (
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 set autocommit=0;
+INSERT INTO `users` VALUES
+(1,'Admin User','admin@example.com','admin123','admin','2025-07-12 04:26:17'),
+(2,'Jon Doe','jon@email.com','jon123','user','2025-07-12 06:13:31'),
+(3,'deno','deno@email.com','deno123','admin','2025-07-12 06:15:23');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 commit;
@@ -86,4 +96,4 @@ commit;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
 
--- Dump completed on 2025-07-11 20:37:19
+-- Dump completed on 2025-07-12 10:21:24
