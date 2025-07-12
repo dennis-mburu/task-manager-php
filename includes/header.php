f122a776577a92daca3575e7e8a754d70a54ca04
Dennis Mburu <dennismburu2063@gmail.com>
2025-07-12 18:43:06 +0300

    test out font-awesome cdn for icon adding

--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,13 @@
 <header>
     <h1>Task Manager</h1>
     <?php if (isset($_SESSION['username'])): ?>
-        <h2>Welcome, <?php echo htmlspecialchars($_SESSION['username']); ?> |
-        <a href="logout.php">Logout</a></h2>
+        <div class=d-flex>
+            <h2>Welcome, <?php echo htmlspecialchars($_SESSION['username']); ?></h2>
+            <button class="logout-button">
+                <a href="logout.php"><i class="fa-solid fa-power-off"></i>Logout</a>
+            </button>
+        </div>
+
     <?php endif; ?>
     <hr>
 </header>
