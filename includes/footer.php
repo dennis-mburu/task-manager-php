e78cd96ac19b90e3dece50fb77ac17768d72695e
Dennis Mburu <dennismburu2063@gmail.com>
2025-07-13 22:40:12 +0300

    style up fppter

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,6 @@
-<footer>
-    <hr>
-    <p>&copy; <?php echo date('Y'); ?> Task Manager</p>
+<footer class="app-footer">
+    <div class="footer-content">
+        <p>&copy; <?php echo date('Y'); ?> Task Manager 🚀</p>
+        <p><i class="fa-solid fa-code"></i> Built by Dennis with 💙</p>
+    </div>
 </footer>
