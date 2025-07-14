c9a88f8e4817f2af0d1e9fd2f1cb2f88bc26cd4f
Dennis Mburu <dennismburu2063@gmail.com>
2025-07-14 07:11:16 +0300

    use infinityFree provided creds for db connection

--- includes/db.php
+++ includes/db.php
@@ -3,13 +3,13 @@
 // This file connects your PHP code to the MySQL database
 
 // Load database credentials from config file (to avoid hardcoding sensitive information)
-$config = parse_ini_file("config.ini");
+// $config = parse_ini_file("config.ini");
 
-// Define DB connection variables
-$host = $config['host'];    // Where your DB server is (localhost if running locally)
-$db_user = $config['db_user'];  // MySQL username (you created this)
-$db_pass = $config['db_pass'];  // MySQL password (leave empty if you haven't set one)
-$db_name = $config['db_name'];  // The name of your database
+// Define infinityFree DB connection variables - provided by infinityFree
+$host = "sql211.infinityfree.com";    // Where your DB server is (localhost if running locally)
+$db_user = "if0_39465344";  // MySQL username
+$db_pass = "XKNxQ3hfSVQztB";  // MySQL password 
+$db_name = "if0_39465344_taskmanager";  // The name of your database
 
 // Connect to the database
 $conn = mysqli_connect($host, $db_user, $db_pass, $db_name);    //Using MySQLi since it's lightweight, built into PHP, and works well for procedural-style apps like this.
@@ -21,4 +21,3 @@ if (!$conn) {
 
 // To verify connection:
 // echo "Connected successfully to database."; // Uncomment this line to test the connection. Do not leave it uncommented in production code as it may expose sensitive information.
-?>
