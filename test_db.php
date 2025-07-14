d90bad0622afa6cae12723cdc13aed0bf60822c0
Dennis Mburu <dennismburu2063@gmail.com>
2025-07-14 09:40:25 +0300

    remove unused files and move mysql dump to root directory

--- test_db.php
+++
@@ -1,5 +0,0 @@
-<?php
-// test_db.php - test file to test database connection.
-include 'includes/db.php'; // This will trigger the connection
-
-echo "✅ Connected successfully to the database! - from test_db.php";
